41ccdcc5637fec07b36b914faedc3faae2b7ad6c
Jay Hesselberth <jay.hesselberth@gmail.com>
2014-02-02 06:04:44 -0700

    Added ipython directive

--- rst/conf.py
+++ rst/conf.py
@@ -32,6 +32,8 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.pngmath',
     'sphinx.ext.mathjax',
+    'IPython.sphinxext.ipython_directive',
+    'IPython.sphinxext.ipython_console_highlighting',
     'hieroglyph',
 ]
 
