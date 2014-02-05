4440d245a23b9a7bd6ba760102974d0a5508012e
Jay Hesselberth <jay.hesselberth@gmail.com>
2014-02-05 06:43:19 -0700

    updated website.sh
    confirm this works

--- rst/website.sh
+++ rst/website.sh
@@ -5,22 +5,30 @@
 # XXX: confirm this is working
 #
 set -o nounset -o pipefail -o errexit -x
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR
+
+RSTROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $RSTROOTDIR
 
 WEBSITEDIR=../../bio-workshop-io/
 BUILDDIR=_build
 
+echo ">> updating content ..."
+cd $WEBSITEDIR
+git pull origin master
+cd $RSTROOTDIR
+echo
+
 echo ">> copying files to $WEBSITEDIR ..."
 cp -r $BUILDDIR/html/* $WEBSITEDIR
 cp -r $BUILDDIR/slides/ $WEBSITEDIR
+echo
 
 echo ">> syncing files with git ..."
 # sync the directory and add new content
 cd $WEBSITEDIR
-git pull origin master
 git add .
 git status
+echo
 
 # use git status to check all is added.
 date=$(date "+%Y-%m-%d at %T")
