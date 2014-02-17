5858ee9e4eed7dd7f2d1b376d0f94644a23e696c
jayhesselberth <jay.hesselberth@gmail.com>
2014-02-17 12:17:29 -0700

    problem set 3 key almost there
    needs debugging of None outputs for answer 2.3

--- problem-set-keys/problem-set-3/results/2014-02-17/problem-2-fastqfile.py
+++ problem-set-keys/problem-set-3/results/2014-02-17/problem-2-fastqfile.py
@@ -1,11 +1,18 @@
 #! /usr/bin/env python
 
-'''Which of the first 10 sequence records has the largest number of
-'C' residues in the sequence? Report its record name'''
+'''
+Problem 2.1 Which of the first 10 sequence records has the largest number of
+'C' residues in the sequence? Report its record name.
+
+Problem 2.2 For each of the first 10 records, Covert each character in the
+quality score to a number, and sum the numbers. Use ord() to convert
+characters to numbers
+
+Problem 2.3
+'''
 
 import sys
 from collections import Counter
-import pdb
 
 if len(sys.argv) != 2:
     print >>sys.stderr, "error: specify fastq file"
@@ -32,21 +39,67 @@ def parse_fastq(fastqfile):
             yield {'name':name, 'seq':seq, 'quals':quals}
             name = seq = quals = None
 
+def reverse_comp(seq):
+    ''' reverse complement a sequence. return in 5'->3' direction '''
+    rc_seq = []
+    for char in reversed(seq):
+        if char == 'T':
+            rc_seq.append('A')
+        elif char == 'A':
+            rc_seq.append('T')
+        elif char == 'G':
+            rc_seq.append('C')
+        if char == 'C':
+            rc_seq.append('G')
+
+    return ''.join(rc_seq)
+
 seen_records = 0
 record_limit = 10
-scores = []
+# problem 2.1 data
+scores_2_1 = []
+
+# problem 2.2 data
+quals_2_2 = []
+
+# problem 2.4 data
+quals_2_4 = []
 
 for record in parse_fastq(open(fastqfilename)):
 
     nuc_counts = Counter(record['seq'])
 
     # add a tuple of (C_count, name) to scores
-    scores.append((nuc_counts['C'], record['name']))
+    scores_2_1.append((nuc_counts['C'], record['name']))
+
+    # problem 2.2 data
+    qual_sum = sum([ord(char) for char in record['quals']])
+    # covert to str for printing later
+    quals_2_2.append(str(qual_sum))
+   
+    quals_2_4.append(reverse_comp(record['seq']))
 
     seen_records += 1
 
     if seen_records == record_limit:
         break
 
-print "the record name with the highest 'C' count is:"
-print max(scores)[1]
+# problem 2.3 data
+seqs = Counter()
+for record in parse_fastq(open(fastqfilename)):
+    seqs[record['seq']] += 1 
+
+print "Problem 2.1: the record name with the highest 'C' count is:"
+print max(scores_2_1)[1]
+
+
+print "Problem 2.2: sums of the first 10 quality scores in ord format:"
+print '\n'.join(quals_2_2)
+
+print "Problem 2.3:  most common sequences in the file:"
+for seq, count in seqs.most_common(10):
+    print seq,'\t',count
+
+print "Problem 2.4: reverse complements of the first 10 seqs:"
+print '\n'.join(quals_2_4)
+
--- problem-set-keys/problem-set-3/results/2014-02-17/run.sh
+++ problem-set-keys/problem-set-3/results/2014-02-17/run.sh
@@ -1,15 +1,16 @@
 #! /usr/bin/env bash
 
-project="$HOME/devel/bio-workshop/problem-set-keys"
-bedfile="$project/problem-set-3/data/2014-02-12/lamina.bed"
-fastqfile="$project/problem-set-3/data/2014-02-17/"
+project="$HOME/devel/bio-workshop/problem-set-keys/problem-set-3"
+bedfile="$project/data/2014-02-12/lamina.bed"
+fastqfile="$project/data/2014-02-17/SP1.fq"
 
-result="$project/results/2014-02-17result.tab"
+result="$project/results/2014-02-17"
+results="$project/results/2014-02-17/result.tab"
 
 if [ ! -d $result ]; then
     mkdir -p $result
 fi
 
-python problem-1-1.py $bedfile >> $result
-python problem-1-2.py $bedfile >> $result
+python problem-1-bedfile.py $bedfile >> $results
+python problem-2-fastqfile.py $fastqfile >> $results
 
