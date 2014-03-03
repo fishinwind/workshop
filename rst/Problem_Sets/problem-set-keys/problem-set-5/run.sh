eebd302b0070fd2d053b03ff082cb073844c058d
jayhesselberth <jay.hesselberth@gmail.com>
2014-03-03 10:14:20 -0700

    ps5 key updates

--- rst/Problem_Sets/problem-set-keys/problem-set-5/run.sh
+++ rst/Problem_Sets/problem-set-keys/problem-set-5/run.sh
@@ -9,6 +9,7 @@
 # XXX this will be user-specific
 project="$HOME/devel/bio-workshop/problem-set-keys/problem-set-5"
 
+# input files
 data="/vol1/opt/data"
 fasta="$data/hg19.fa"
 dnase_bed="$data/wgEncodeRegDnaseClusteredV2.bed.gz"
@@ -16,16 +17,20 @@ ctcf_bed="$data/wgEncodeUwTfbsHelas3CtcfStdPkRep1.narrowPeak.gz"
 clustered_tfbs_bed="$data/wgEncodeRegTfbsClusteredV3.bed.gz"
 
 result="$project/results/2014-03-03"
-results="$result/result.tab"
 
 if [ ! -d $result ]; then
     mkdir -p $result
 fi
 
-# Problem 1.1, 1.2
+# output files
 ctcf_fasta="$result/ctcf.chr22.fa"
 ctcf_chr22_bed="$results.ctcf.chr22.bed"
+tfbs_no_dhs="$result/tfbs_no_dhs.bed"
+tfbs_no_dhs_summary="$result/tfbs_no_dhs.summary.tab"
+dhs_no_tfbs="$result/dhs_no_tfbs.bed"
+dhs_no_tfbs_fasta="$result/dhs_no_tfbs.fa"
 
+# Problem 1.1, 1.2
 awk '$1 == chr22' < $ctcf_bed > $ctcf_chr22_bed
 bedtools getfasta -fi $fasta -bed $ctcf_chip -fo $ctcf_fasta
 
@@ -36,25 +41,21 @@ meme -nmotifs 5 -dna -minw 6 -maxw 20 -maxsize 100000000 \
 # Identify transcription factor binding peaks that do not overlap with
 # DNase I hypersensitive sites
 
-tfbs_no_dhs="$result/tfbs_no_dhs.bed"
 bedtools intersect -a $clustered_tfbs_bed -b $dnase_bed -v -sorted \
     > $tfbs_no_dhs
 
 # What transcription factors are represented in these peaks?
-tfbs_no_dhs_summary="$result/tfbs_no_dhs.summary.tab"
 zcat $tfbs_no_dhs | cut -f4 | sort | uniq -c | sort -k1nr \
     > $tfbs_no_dhs_summary
 
 # identify DNase I hypersensitive sites that do not have corresponding
 # transcription factor peak calls
-dhs_no_tfbs="$result/dhs_no_tfbs.bed"
 bedtools intersect -a $dnase_bed -b $clustered_tfbs_bed -v -sorted \
     > $dhs_no_tfbs
 
 # What motifs are enriched in this set of hypersensitive sites?
-dhs_no_tfbs_fasta="$result/dhs_no_tfbs.fa"
 bedtools getfasta -fi $fasta -bed $dhs_no_tfbs -fo $dhs_no_tfbs_fasta
-meme -nmotifs 10 -dna -minw 6 -maxw 20 -maxsize 100000000 \
-    -o problem_2_2_2_motifs $dhs_no_tfbs_fasta
 
-    
+meme -nmotifs 10 -dna -minw 6 -maxw 20 -maxsize 100000000 \
+    -o problem_2_motifs $dhs_no_tfbs_fasta
+
