212c8ce3e5c4dfcbb0e100398f7669074e54ebab
jayhesselberth <jay.hesselberth@gmail.com>
2014-03-06 09:56:35 -0700

    improved analysis

--- rst/Classes/code/analysis.py
+++ rst/Classes/code/analysis.py
@@ -1,5 +1,8 @@
 #! /usr/bin/env python
 
+'''
+
+'''
 import sys
 import pdb
 import random
@@ -22,22 +25,32 @@ def get_cell_type(dirname):
 filenames = sys.argv[1:]
 subset = random.sample(filenames, 10)
 
-header_fields = ['#cell.type.1','cell.type.2','jaccard']
+cons_bedgraph = BedTool('/vol1/opt/data/hg19.100way.phyloP100way.bg.gz')
+
+header_fields = ['#cell.type.1','cell.type.2','type','jaccard','mean.cons']
 print '\t'.join(header_fields)
 
 for fname1, fname2 in combinations(subset, r=2):
 
-    tool1 = BedTool(fname1)
-    tool2 = BedTool(fname2)
-
-    result = tool1.jaccard(tool2, f=0.5, r=False)    
-    stat = result['jaccard']
-
     ctype1 = get_cell_type(fname1)
     ctype2 = get_cell_type(fname2)
 
-    fields = [ctype1, ctype2, stat]
+    tool1 = BedTool(fname1)
+    tool2 = BedTool(fname2)
 
-    # print '\t'.join(fields)
+    # jaccard statistic
+    result = tool1.jaccard(tool2, f=0.5, r=False)    
+    stat = result['jaccard']
+
+    # conservation measurement
+    result1 = tool1.intersect(tool2, v=True)    
+    result2 = tool2.intersect(tool1, v=True)    
+
+    cons1 = result1.map(cons_bedgraph, o='mean', c=4)
+    cons2 = result2.map(cons_bedgraph, o='mean', c=4)
+
+    pbd.set_trace()
+
+    fields = [ctype1, ctype2, stat]
     print '\t'.join(map(str, fields))
 
