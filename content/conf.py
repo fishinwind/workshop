77d38d81cc97dbfb502fb908404df1b526f14d41
jayhesselberth <jay.hesselberth@gmail.com>
2015-02-16 13:44:31 -0700

    update copyr

--- content/conf.py
+++ content/conf.py
@@ -89,7 +89,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Genome Analysis Workshop'
-copyright = u'2014, Jay Hesselberth'
+copyright = u'2013-2015, Jay Hesselberth'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
